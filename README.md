16c42f65f9ac9c275d0fc170614b6e8f3e18473c
Akki Khare <admin@akki.io>
2020-11-29 23:36:39 -0500

    Updated readme and add stable release script

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Laravel Docker Images
 
+[![Docker Badge](https://img.shields.io/docker/pulls/akkica/laravel-web)](https://hub.docker.com/u/akkica)
+
 This repository complements the tutorial, which shows how to get started with your own Laravel Docker Images. Click [here](https://akki.ca/tutorial/laravel-docker) to learn more.
+
+| Tag  |  PHP version |
+|---|---|
+| 7.1 | 7.1 |
+| 7.2 | 7.1 |
+| 7.3 | 7.1 |
+| 7.4 | 7.1 |
+| 8.0rc | 8.0-rc |
+| stable | 7.4 |
+| latest | 8.0-rc |
+
+Visit [https://akki.ca/tutorial/laravel-docker/](https://akki.ca/tutorial/laravel-docker/) to learn more.
---
+++ _stable.sh
@@ -0,0 +1,27 @@
+## Find and replace the version
+
+## laravel
+cd laravel-base \
+  && docker build -t akkica/laravel-base:stable . \
+  && docker push akkica/laravel-base:stable \
+  && cd ..
+
+cd laravel-web \
+  && docker build -t akkica/laravel-web:stable . \
+  && docker push akkica/laravel-web:stable \
+  && cd ..
+
+cd laravel-cron \
+  && docker build -t akkica/laravel-cron:stable . \
+  && docker push akkica/laravel-cron:stable \
+  && cd ..
+
+cd laravel-worker \
+  && docker build -t akkica/laravel-worker:stable . \
+  && docker push akkica/laravel-worker:stable \
+  && cd ..
+
+cd laravel-horizon \
+  && docker build -t akkica/laravel-horizon:stable . \
+  && docker push akkica/laravel-horizon:stable \
+  && cd ..
