0c971d29bb93f56d7d2174c5957d7720529bec31
Akki Khare <admin@akki.io>
2021-12-18 15:55:04 -0500

    update to php8.1

--- README.md
+++ README.md
@@ -4,15 +4,16 @@
 
 This repository complements the tutorial, which shows how to get started with your own Laravel Docker Images. Click [here](https://akki.ca/tutorial/laravel-docker) to learn more.
 
-| Tag  |  PHP version | Status |
-|---|---|---|
-| 7.1 | 7.1 | End of Life |
-| 7.2 | 7.2 | End of Life |
-| 7.3 | 7.3 | End of Life |
-| 7.4 | 7.4 | Security fixes only |
-| 8.0rc | 8.0-rc | End of Life |
-| 8.0 | 8.0 | All fixes |
-| stable | 8.0 | All fixes |
-| latest | 8.0 | All fixes |
+| Tag    | PHP version | Status |
+|--------|-------------|---|
+| 7.1    | 7.1         | End of Life |
+| 7.2    | 7.2         | End of Life |
+| 7.3    | 7.3         | End of Life |
+| 7.4    | 7.4         | Security fixes only |
+| 8.0rc  | 8.0-rc      | End of Life |
+| 8.0    | 8.0         | All fixes |
+| 8.1    | 8.1         | All fixes |
+| stable | 8.1         | All fixes |
+| latest | 8.1         | All fixes |
 
 Visit [https://akki.ca/tutorial/laravel-docker/](https://akki.ca/tutorial/laravel-docker/) to learn more.
--- _update.sh
+++ _update.sh
@@ -2,31 +2,31 @@
 
 ## laravel
 cd laravel-base \
-  && docker build -t akkica/laravel-base:8.0 -t akkica/laravel-base:latest . \
-  && docker push akkica/laravel-base:8.0 && docker push akkica/laravel-base:latest \
+  && docker build -t akkica/laravel-base:8.1 -t akkica/laravel-base:latest . \
+  && docker push akkica/laravel-base:8.1 && docker push akkica/laravel-base:latest \
   && cd ..
 
 cd laravel-web \
-  && docker build -t akkica/laravel-web:8.0 -t akkica/laravel-web:latest . \
-  && docker push akkica/laravel-web:8.0 && docker push akkica/laravel-web:latest \
+  && docker build -t akkica/laravel-web:8.1 -t akkica/laravel-web:latest . \
+  && docker push akkica/laravel-web:8.1 && docker push akkica/laravel-web:latest \
   && cd ..
 
 cd laravel-web-wkhtmltopdf \
-  && docker build -t akkica/laravel-web-wkhtmltopdf:8.0 -t akkica/laravel-web-wkhtmltopdf:latest . \
-  && docker push akkica/laravel-web-wkhtmltopdf:8.0 && docker push akkica/laravel-web-wkhtmltopdf:latest \
+  && docker build -t akkica/laravel-web-wkhtmltopdf:8.1 -t akkica/laravel-web-wkhtmltopdf:latest . \
+  && docker push akkica/laravel-web-wkhtmltopdf:8.1 && docker push akkica/laravel-web-wkhtmltopdf:latest \
   && cd ..
 
 cd laravel-cron \
-  && docker build -t akkica/laravel-cron:8.0 -t akkica/laravel-cron:latest . \
-  && docker push akkica/laravel-cron:8.0 && docker push akkica/laravel-cron:latest \
+  && docker build -t akkica/laravel-cron:8.1 -t akkica/laravel-cron:latest . \
+  && docker push akkica/laravel-cron:8.1 && docker push akkica/laravel-cron:latest \
   && cd ..
 
 cd laravel-worker \
-  && docker build -t akkica/laravel-worker:8.0 -t akkica/laravel-worker:latest . \
-  && docker push akkica/laravel-worker:8.0 && docker push akkica/laravel-worker:latest \
+  && docker build -t akkica/laravel-worker:8.1 -t akkica/laravel-worker:latest . \
+  && docker push akkica/laravel-worker:8.1 && docker push akkica/laravel-worker:latest \
   && cd ..
 
 cd laravel-horizon \
-  && docker build -t akkica/laravel-horizon:8.0 -t akkica/laravel-horizon:latest . \
-  && docker push akkica/laravel-horizon:8.0 && docker push akkica/laravel-horizon:latest \
+  && docker build -t akkica/laravel-horizon:8.1 -t akkica/laravel-horizon:latest . \
+  && docker push akkica/laravel-horizon:8.1 && docker push akkica/laravel-horizon:latest \
   && cd ..
