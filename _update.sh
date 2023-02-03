fccbbadf55b9dcb91c6fe6be4787347e42382591
Akki Khare <admin@akki.io>
2023-02-02 19:53:14 -0500

    fix: image

--- _update.sh
+++ _update.sh
@@ -2,31 +2,31 @@
 
 ## laravel
 cd laravel-base \
-  && docker build -t akkica/laravel-base:7.4 -t akkica/laravel-base:latest . \
-  && docker push akkica/laravel-base:7.4 && docker push akkica/laravel-base:latest \
+  && docker build -t akkica/laravel-base:7.4 . \
+  && docker push akkica/laravel-base:7.4   \
   && cd ..
 
 cd laravel-web \
-  && docker build -t akkica/laravel-web:7.4 -t akkica/laravel-web:latest . \
-  && docker push akkica/laravel-web:7.4 && docker push akkica/laravel-web:latest \
+  && docker build -t akkica/laravel-web:7.4 . \
+  && docker push akkica/laravel-web:7.4   \
   && cd ..
 
 cd laravel-web-wkhtmltopdf \
-  && docker build -t akkica/laravel-web-wkhtmltopdf:7.4 -t akkica/laravel-web-wkhtmltopdf:latest . \
-  && docker push akkica/laravel-web-wkhtmltopdf:7.4 && docker push akkica/laravel-web-wkhtmltopdf:latest \
+  && docker build -t akkica/laravel-web-wkhtmltopdf:7.4 . \
+  && docker push akkica/laravel-web-wkhtmltopdf:7.4   \
   && cd ..
 
 cd laravel-cron \
-  && docker build -t akkica/laravel-cron:7.4 -t akkica/laravel-cron:latest . \
-  && docker push akkica/laravel-cron:7.4 && docker push akkica/laravel-cron:latest \
+  && docker build -t akkica/laravel-cron:7.4 . \
+  && docker push akkica/laravel-cron:7.4   \
   && cd ..
 
 cd laravel-worker \
-  && docker build -t akkica/laravel-worker:7.4 -t akkica/laravel-worker:latest . \
-  && docker push akkica/laravel-worker:7.4 && docker push akkica/laravel-worker:latest \
+  && docker build -t akkica/laravel-worker:7.4 . \
+  && docker push akkica/laravel-worker:7.4   \
   && cd ..
 
 cd laravel-horizon \
-  && docker build -t akkica/laravel-horizon:7.4 -t akkica/laravel-horizon:latest . \
-  && docker push akkica/laravel-horizon:7.4 && docker push akkica/laravel-horizon:latest \
+  && docker build -t akkica/laravel-horizon:7.4 . \
+  && docker push akkica/laravel-horizon:7.4   \
   && cd ..
